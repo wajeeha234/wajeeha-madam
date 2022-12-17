e5b5a01be6361bb4e90fdbd4e4cd80d4fae46150
haziq javed <haziqmalik320@gmail.com>
2022-12-17 15:46:29 +0500

    3rd comment

---
+++ colorfulpacman.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+main()
+{
+system("color 06");
+cout<< " ####  ###        #            ##  ###    #             #          #          #                 # "<<endl;
+cout<< "   ##      ##    # #         ##           #  #       #  #         # #         # #               # "<<endl;
+cout<< "   ##       ##  #   #        ##           #    #   #    #        #   #        #   #             # "<<endl;
+cout<< "   ##      ##  #     #       ##           #      #      #       #     #       #     #           # "<<endl;
+cout<< "   ##  ###    #########      ##           #             #      #########      #       #         # "<<endl;      
+cout<< "   ##        #         #     ##           #             #     #         #     #         #       # "<<endl;
+cout<< "   ##       #           #    ##           #             #    #           #    #           #     # "<<endl;
+cout<< "   ##      #             #   ##           #             #   #             #   #             #   # "<<endl;
+cout<< "   ##     #               #  ##           #             #  #               #  #               # # "<<endl;
+cout<< "   ##    #                 #   ##  ###    #             # #                 # #                 # "<<endl;
+return 0;
+}
